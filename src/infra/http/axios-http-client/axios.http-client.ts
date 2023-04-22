6bf3b98dbf9a5a6589e9346c3a119c3784205eac
Wagão <wcfx.dev@gmail.com>
2023-04-22 00:37:09 -0300

    feat: ensure AxiosHttpCliente calls axios with correct body

--- src/infra/http/axios-http-client/axios.http-client.ts
+++ src/infra/http/axios-http-client/axios.http-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 
 export class AxiosHttpClient {
   async post(params: TRequestHttpPostParams<any>): Promise<void> {
-    await axios.post(params.url);
+    await axios.post(params.url, params.body);
   }
 }
